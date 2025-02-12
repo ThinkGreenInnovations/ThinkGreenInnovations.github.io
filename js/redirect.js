0b344b9d9f62094b3c9943f058a131542313adc9
SirAlchemist <nickstarster@gmail.com>
2025-02-12 17:58:55 -0400

    Updated links in redirect

--- js/redirect.js
+++ js/redirect.js
@@ -3,14 +3,14 @@
     window.location.href = "html/home.html";
 
     // Load the header and footer includes
-    fetch('includes/Header.html')
+    fetch('includes/header.html')
             .then(response => response.text())
             .then(data => {
         document.getElementById('header').innerHTML = data;
             })
             .catch(error => console.error('Error loading header:', error));
 
-    fetch('includes/Footer.html')
+    fetch('includes/footer.html')
             .then(response => response.text())
             .then(data => {
         document.getElementById('footer').innerHTML = data;
