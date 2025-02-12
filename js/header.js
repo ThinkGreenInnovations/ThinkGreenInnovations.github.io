983d0283a77907918a5c046a6d614d7ce9754a2c
SirAlchemist <nickstarster@gmail.com>
2025-02-12 18:36:50 -0400

    Updated paths

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('/includes/header.html')
+    fetch('/html/includes/header.html')
         .then(response => response.text())
         .then(data => document.querySelector('header').innerHTML = data);
 
