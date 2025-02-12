55dda27083a7771760095193b41462a0ae0174f1
SirAlchemist <nickstarster@gmail.com>
2025-02-12 15:58:53 -0400

    Ensuring continuity

---
+++ js/header.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburger.addEventListener('click', function () {
+        mobileMenu.classList.toggle('open'); // Toggles open class on mobile menu
+        this.classList.toggle('active'); // Toggles active class on hamburger
+    });
+
+    // Adding event listener for links to close mobile menu on click
+    const mobileLinks = document.querySelectorAll('.mobile-menu a');
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            mobileMenu.classList.remove('open');
+            hamburger.classList.remove('active');
+        });
+    });
+});
---
+++ js/redirect.js
@@ -0,0 +1,19 @@
+<script>
+        // JavaScript Redirect
+    window.location.href = "html/Home.html";
+
+    // Load the header and footer includes
+    fetch('includes/Header.html')
+            .then(response => response.text())
+            .then(data => {
+        document.getElementById('header').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading header:', error));
+
+    fetch('includes/Footer.html')
+            .then(response => response.text())
+            .then(data => {
+        document.getElementById('footer').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading footer:', error));
+</script>
