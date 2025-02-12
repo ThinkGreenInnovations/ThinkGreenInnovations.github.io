d2a80132aeff26356c40525723056193b325c7d0
SirAlchemist <nickstarster@gmail.com>
2025-02-12 17:51:38 -0400

    Adjusted header and footer

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('../includes/footer.html')
+    fetch('/includes/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
